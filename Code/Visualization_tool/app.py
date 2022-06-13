bfd93778796fca76cec82010b8bcf595442c01b8
okivlo <i.vloothuis@gmail.com>
2022-06-13 17:51:02 +0200

    Added aggregation function and dataframe loader

--- Code/Visualization_tool/app.py
+++ Code/Visualization_tool/app.py
@@ -1,59 +1,109 @@
-from dash import html, dcc
-from dash import Dash
-from dash.dependencies import Input, Output
-import geopandas as gpd
+import geojson
 import plotly.express as px
-import json
+from dash import Dash, dcc, html
 from pandas import read_pickle
-import plotly.express as px
-import pandas as pd
-
-import json
-from pandas import read_pickle
-import plotly.express as px
-import shapefile
-import pandas as pd
 
 
-def load_shapefile():
-    geojson_data = gpd.read_file("new.json")
-    geojson_data = geojson_data.to_crs(4326).__geo_interface__
+def load_geojson():
+    """
+    Opens the the geojson file and returns it. The file is assumed to be in the correct coordinate system.
+    :return:geojson file with municipalities.
+    """
+
+    with open("new.geojson") as f:
+        geojson_data = geojson.load(f)
     return geojson_data
 
 
-
-
-def load_json():
-
-    geojson_data = shapefile.Reader("../../gemeente_lines/gemeente_2020_v2.shp").__geo_interface__
-    return geojson_data
-
-
-def load_dataset(path):
+def aggregate_dataframe(dataframe, factor):
     """
-    Loads a dataset in.
-    :param path: path to dataset.
-    :return: Unpickled dataset.
+    This function is called whenever we have more than one year selected.
+    In that case we need to aggregate the data of those years.  At least for the moves it should be summed.
+    It assumes that the dataframe has already thrown out the year that are not relevant.
+    :param dataframe: The dataframe that needs aggregated data (Pandas df)
+    :param factor: The chosen factor. (string with '_other' appended)
+    :return: pandas dataframe with the moves columns aggregated.
     """
-    data = read_pickle(path)
-    return data
+    # TODO: How exactly do we want to aggregate the other columns. I believe that the latest variable is actually fine.
+    #  Maybe we want a difference score? it can be adapted in header_agg
 
-def make_choropleth(geojson, dataframe):
+    # Aggregation options. Sum for moves and last for the chosen factor. can be changed.
+    header_agg = {'moves': 'sum',
+                  factor: 'last'
+                  }
 
-    idx = dataframe[(dataframe['RegioS'] != "GM0363")].index
-    dataframe.drop(idx, inplace=True)
-    print(dataframe)
+    # This is where the aggregation happens:
+    aggregated_dataframe = dataframe.groupby(['gemeente_code', 'gemeente_naam', 'year']).agg(header_agg).reset_index()
 
-    df = dataframe
-    df.astype({"RegioS": str})
-    geojson = geojson
+    return aggregated_dataframe
 
+
+def prepare_dataset(city, to_from, factor, years):
+    """
+    Loads a dataset in and filters it based on the criteria passed.
+    :param city: The chosen city from the top 10. (string, with capital)
+    :param to_from: Whether we want data to or from that city. (string, either To or From, with capitals)
+    :param factor: Which factor we want to display. (String + '_other')
+    :param years: Which years are chosen. (List)
+    :return: Unpickled dataset with only relevant columns
+    """
+
+    # Construct path to dataset depending on inputs.
+    base_df_path = '../../QueryDFs/'
+    df_path = base_df_path + to_from + '_' + city + '.pkl'
+    df = read_pickle(df_path)
+
+    # Drop irrelevant columns unless specified that we want all columns
+    if factor != 'all':
+        df = df[['gemeente_code', 'gemeente_naam', 'year', 'moves', factor]]
+
+    # Drop irrelevant years
+    df = df.loc[(df['year'].isin(years))]
+
+    # If the user wants more than one year, we need to aggregate the data of those years
+    if len(years) > 1:
+        df = aggregate_dataframe(dataframe=df, factor=factor)
+
+    return df
+
+
+def make_choropleth(geojson_file, city='Amsterdam', to_from='To', factor='prices', years=None, sorting=True):
+    """
+    Function to make the actual choropleth figure.
+    Should be called on whenever the user presses an update button somewhere in the dash_app.
+    :param geojson_file: A geojson file with the municipality data. (geojson)
+    :param city: One of the top 10 cities. (string, with capital letter)
+    :param to_from: Whether we want data to or from that city. (string, either To or From, with capitals)
+    :param factor: Which factor we want to display. (String, no capital letters)
+    :param years: Which years are chosen. (List of ints)
+    :param sorting: A boolean, signifying whether we want to color the choropleth based on moves (True)
+                    or based on the chosen factor (False).
+    :return: Returns a figure.
+    """
+
+    # This is just a default parameter, because i don't know how dash updates itself.
+    if years is None:
+        years = [2016]
+
+    # I believe bram constructed the column name to be this format, so we need to change factor
+    factor = factor + '_other'
+
+    # Prepare dataset according to the chosen parameters
+    df = prepare_dataset(city=city, to_from=to_from, factor=factor, years=years)
+
+    # Decide what factor to base the choropleth colouring on
+    if sorting:
+        color = 'moves'
+    else:
+        color = factor + '_other'
+
+    # Make the actual choropleth
     fig = px.choropleth_mapbox(df,
-                               geojson=geojson,
-                               color="GemiddeldeWoningwaarde_4",
+                               geojson=geojson_file,
+                               color=color,
                                locations="RegioS",
                                featureidkey="properties.statcode",
-                               range_color=[min(df['GemiddeldeWoningwaarde_4']), max(df['GemiddeldeWoningwaarde_4'])],  
+                               range_color=[min(df[factor]), max(df[factor])],
                                zoom=9,
                                center={"lat": 52.370216, "lon": 4.895168},
                                mapbox_style="carto-positron",
@@ -62,15 +112,16 @@ def make_choropleth(geojson, dataframe):
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     fig.update_geos(fitbounds="locations", visible=False)
 
-    print("Preparing to show figure: ")
-    # fig.show()
     return fig
 
 
-def main(fig):
+def main(geojson_file):
+
+    # Initialize a figure? I am not sure if this is needed
+    fig = make_choropleth(geojson_file=geojson_file)
+
     app = Dash(__name__)
     app.title = "Knowledge Engineering Visualization"
-    # fig = px.choropleth_mapbox(data_frame = load_shapefile(), featureidkey = "GM_CODE", )
     app.layout = html.Div([
         html.H1('Visualization of Movement in the Netherlands'),
         html.H3('Select the City'),
@@ -104,16 +155,15 @@ def main(fig):
         html.Div(
             [dcc.Graph(id='example-graph-1', figure=fig)])])
 
-    # Callbacks maken dat hij update op selectie
+    # Make callbacks to update selection
 
     app.run_server(debug=False, dev_tools_ui=False)
+    # TODO: Hier zouden die callbacks moeten plaatsvinden om make_choropleth aan te roepen. Geen idee hoe.
+    #  Het format om make_choropleth aan te roepen is the vinden in de desbetreffende docstring.
+
+
 
 
 if __name__ == '__main__':
-    # j_file = load_json()
-    j_file = load_shapefile()
-    dataset = load_dataset("../../cleanedDFs/prices.pkl")
-    figure = make_choropleth(j_file, dataset)
-    main(figure)
-    #figure.show()
-    #main(figure)
+    j_file = load_geojson()
+    main(j_file)
